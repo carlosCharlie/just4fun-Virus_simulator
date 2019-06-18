590a59e3ae5a3009d907c7d8ed606f0371024f66
carlosCharlie <carlosc21c21@gmail.com>
2019-06-18 12:27:56 +0200

    .

--- js/main.js
+++ js/main.js
@@ -59,7 +59,8 @@ window.onload = function(){
             y:(((country.getBoundingClientRect().height/2)+country.getBoundingClientRect().top)*620)/map.getBoundingClientRect().height
             });
 
-            countries[country.id].html.ontouchstart = function(event){
+            countries[country.id].html.onclick = function(event){
+                event.preventDefault();
                 countries[event.target.id].setPatientZero();
                 event.target.classList.add("clicked");
                 document.getElementById("click").style.display="none";
